6d4bb73f20b0bc9a8b60260db06c184eb5e10efc
ljheayeee <ljheayee@naver.com>
2022-09-26 22:17:31 +0900

    first java project

---
+++ firstProject/src/com/test/first/Hello.java
@@ -0,0 +1,13 @@
+package com.test.first;
+
+public class Hello {
+	/*
+	 * main method: 프로그램의 진입점, 시작점 메인 메소드가 하나여야 시작 된
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+	}
+
+
+}
